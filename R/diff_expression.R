d1635e74aa4084555acb42959f3194e9cd27ecf1
jonathonthill <jhill@byu.edu>
2024-07-17 13:23:11 -0600

    created parent function for pval calc.

--- R/diff_expression.R
+++ R/diff_expression.R
@@ -1,4 +1,5 @@
-.fisher_pval <- function (q, m, n, k) {
+.fast_fisher_pval <- function (q, m, n, k) 
+{
   # derived from https://github.com/al2na/methylKit/issues/96
   
   mat <- cbind(q, m, n, k)
@@ -24,7 +25,8 @@
 .calc_diff_pos_fisher <- function(modseq_dat, 
                                   case, 
                                   control, 
-                                  mod_type = "mh") 
+                                  mod_type,
+                                  calc_type) 
 {
   # Set stat to use
   mod_counts_col <- paste0(mod_type[1], "_counts")
@@ -51,24 +53,43 @@
       values_from = c(c_counts, mod_counts),
       values_fill = 0) 
   
-  # Extract Matrix and calculate pvals
+  # Extract matrix
   pvals <- 
     dat |>
     select(!chrom:ref_position) |>
     distinct() |>
-    collect() |>
     mutate(
       mod_frac_case = mod_counts_case /
-        sum(mod_counts_case, c_counts_case),
+        (mod_counts_case + c_counts_case),
       mod_frac_control = mod_counts_control /
-        sum(mod_counts_control, c_counts_control),
-      meth_diff = mod_counts_case / sum(mod_counts_case, c_counts_case) -
-        mod_counts_control / sum(mod_counts_control, c_counts_control),
-      p_val = .fisher_pval(
-        q = mod_counts_case,
-        m = mod_counts_case + mod_counts_control,
-        n = c_counts_case + c_counts_control,
-        k = mod_counts_case + c_counts_case)) 
+        (mod_counts_control + c_counts_control),
+      meth_diff = mod_counts_case / 
+        (mod_counts_case + c_counts_case) -
+        mod_counts_control / 
+        (mod_counts_control + c_counts_control)) |>
+    collect()
+  
+  # Calculate pvals
+  if (calc_type == "fast_fisher") {
+    pvals <- 
+      pvals |>
+      mutate(
+        p_val = .fisher_pval(
+          q = mod_counts_case,
+          m = mod_counts_case + mod_counts_control,
+          n = c_counts_case + c_counts_control,
+          k = mod_counts_case + c_counts_case)) 
+  } else if (calc_type == "r_fisher") {
+    cols <- c("c_counts_control", "c_counts_case", 
+              "mod_counts_control", "mod_counts_case")
+    pvals <- 
+      pvals |>
+      rowwise() |>
+      mutate(
+        p_val = fisher.test(matrix(c_across(cols), 2))$p.val)
+  } else {
+    stop("Invalid calc_type argument.")
+  }
   
   dat |>
     inner_join(
@@ -80,43 +101,21 @@
       ~ gsub("mod", mod_type[1], .x))
 }
 
-.calc_diff_pos_fisher2 <- function(modseq_dat, 
-                                  case, 
-                                  control, 
-                                  mod_type = "mh") 
+calc_diff_meth <- function(modseq_dat, 
+                           cases, 
+                           controls, 
+                           mod_type = "mh",
+                           calc_type = "fast_fisher") 
 {
-  # Set stat to use
-  mod_counts_col <- paste0(mod_type[1], "_counts")
-  
-  # Select necessary columns
-  modseq_dat |>
-    select(
-      sample_name, chrom, ref_position, c_counts, mod_counts = !!mod_counts_col) |>
-    mutate(
-      exp_group = case_when(
-        sample_name %in% case ~ "case",
-        sample_name %in% control ~ "control",
-        TRUE ~ NA)) |>
-    filter(
-      !is.na(exp_group)) |>
-    summarize(
-      .by = c(exp_group, chrom, ref_position),
-      c_counts = sum(c_counts),
-      mod_counts = sum(mod_counts)) |>
-    pivot_wider(
-      id_cols = c(chrom, ref_position),
-      names_from = exp_group, 
-      values_from = c(c_counts, mod_counts),
-      values_fill = 0) |>
-    mutate(
-      .by = c(chrom, ref_position),
-      p_val = fisher.test(matrix(c_across(everything()), 2))$p.val,
-      mod_frac_case = mod_counts_case /
-        sum(mod_counts_case, c_counts_case),
-      mod_frac_control = mod_counts_control /
-        sum(mod_counts_control, c_counts_control),
-      meth_diff = mod_counts_case / sum(mod_counts_case, c_counts_case) -
-        mod_counts_control / sum(mod_counts_control, c_counts_control)) |>
-    rename_with(
-      ~ gsub("mod", mod_type[1], .x))
+  switch(calc_type,
+         fast_fisher = .calc_diff_pos_fisher(modseq_dat,
+                                             cases,
+                                             controls,
+                                             mod_type,
+                                             calc_type = "fast_fisher"),
+         r_fisher    = .calc_diff_pos_fisher(modseq_dat,
+                                             cases,
+                                             controls,
+                                             mod_type,
+                                             calc_type = "r_fisher"))
 }
