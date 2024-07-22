c83bbe87676ef9539f1610f04ab9a06722d95905
jonathonthill <jhill@byu.edu>
2024-07-22 11:25:42 -0600

    performance improvements

--- R/summarize_by_region.R
+++ R/summarize_by_region.R
@@ -1,34 +1,37 @@
-summarize_by_region <- function(modseq_dat, 
-                                annot_file)
+aggregate_regions <- function(modseq_dat, 
+                              annot_file)
 {
   # Read annotation
   annotation <- 
-    read_tsv(annot_file, col_names = c("chrom", "start", "end"))
+    read_tsv(annot_file, 
+             col_names = c("chrom", "start", "end", "region_name"), 
+             show_col_types = FALSE) 
   
-  # Make sure annotation file is formatted correctly
-  if (ncol(annotation) < 3 || ncol(annotation) > 4) {
-    stop("Annotation must be in correct format. 
-         ex. chrom, start, end, region_name OR chrom, start, end")
+  stopifnot("Invalid annotation format. File must have chr, start, end." = 
+              ncol(annotation) >= 3)
+  
+  if (ncol(annotation == 3)) {
+    annotation <- 
+      annotation |>
+      mutate(
+        region_name = paste(chrom, start, end, sep = "_"))
   }
   
+  annotation <- 
+    annotation |>
+    reframe(
+      .by = c(region_name, chrom),
+      ref_position = start:end)
+    
   # Create regional dataframe
-  modseq_dat <- 
-    right_join(
-      modseq_dat, annotation, 
-      by = join_by(chrom, between(ref_position, start, end)),
-      copy = TRUE)
-  
-  # Calculate the requested scores- summarize scores based on available columns
   modseq_dat |>
+    right_join(
+      annotation, 
+      by = join_by(chrom, ref_position),
+      copy = TRUE) |>
     summarize( 
       .by = c(sample_name, region_name),
-      mean_mh_frac = if_else("mh_frac" %in% colnames(modseq_dat), 
-                             sum(cov * mh_frac) / sum(cov), NA_real_),
-      mean_m_frac = if_else("m_frac" %in% colnames(modseq_dat), 
-                            sum(cov * m_frac) / sum(cov), NA_real_),
-      mean_h_frac = if_else("h_frac" %in% colnames(modseq_dat), 
-                            sum(cov * h_frac) / sum(cov), NA_real_),
-      mean_cov = mean(cov, na.rm = TRUE)) |>
-    select_if(
-      ~ !all(is.na(.)))
+      cov = sum(cov),
+      across(ends_with("_counts"), sum),
+      across(ends_with("_frac"), ~ sum(.x * cov) / sum(cov)))
 }
